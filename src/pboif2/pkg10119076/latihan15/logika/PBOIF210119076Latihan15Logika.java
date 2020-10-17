3a11243d0aeb44540e0668a154efa4d0a5cc3b46
ACER <ACER@DESKTOP-JMQ2UKH>
2020-10-17 17:42:00 +0700

    PBOIF2-10119076-Latihan15-Logika

---
+++ src/pboif2/pkg10119076/latihan15/logika/PBOIF210119076Latihan15Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119076.latihan15.logika;
+
+/**
+ *
+ * @author ACER
+ * NAMA     : Sulthon Naufal Akmal
+ * KELAS    : IF2
+ * NIM      : 10119076
+ * Deskripsi Program : Logika
+ */
+public class PBOIF210119076Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
